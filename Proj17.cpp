e7eaf5e414822249ab765023adf361a00f631df3
Zohaib Ashraf <zohaibashraf2559@gmail.com>
2025-08-12 15:56:20 +0500

    Add files via upload

---
+++ Proj17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    float a, b, result;
+
+    cout << "Enter first number: ";
+    cin >> a;
+                                                               
+    cout << "Enter second number: ";
+    cin >> b;
+
+    result = a - b;
+    cout << "The Result is: " <<result <<endl;
+
+    return 0;                                                                              
+}
